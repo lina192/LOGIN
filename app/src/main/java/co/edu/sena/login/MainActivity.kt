a4f009c19ace088316fbcd317cda7e3a3dd71dda
lina192 <pe8708018@gmail.com>
2024-07-21 15:37:39 -0500

    feat:Se creo el entorno de trabajo

---
+++ app/src/main/java/co/edu/sena/login/MainActivity.kt
@@ -0,0 +1,25 @@
+package co.edu.sena.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login.app.PostOfficeApp
+import co.edu.sena.login.ui.theme.LoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
