2944f9862f7321672ae959bc971539ce5f4d0e6b
lina192 <pe8708018@gmail.com>
2024-07-21 21:47:28 -0500

    feat: Este commit creo y añido contenido a la clase PostOfficeApp

---
+++ app/src/main/java/co/edu/sena/login/app/PostOfficeApp.kt
@@ -0,0 +1,20 @@
+package co.edu.sena.login.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun PostOfficeApp(){
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+
+    ) {
+
+    }
+}
+
